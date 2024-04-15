6fb8c24da0f124ceb19c90bbfeeb4591e2ed02bf
Bow Jaro <100690848+BowJaro@users.noreply.github.com>
2024-04-15 10:13:43 +0700

    Create TransactionWithName no handle event click

--- wise-money/app/(tabs)/home/index.js
+++ wise-money/app/(tabs)/home/index.js
@@ -1,6 +1,8 @@
 import { Stack } from "expo-router";
 import { StyleSheet, Text, View } from "react-native";
 import TransactionWithDate from '../../../components/transaction/TransactionWithDate'
+import TransactionWithName from '../../../components/transaction/TransactionWithName'
+
 export default function Page() {
   const transaction = {
     id: 1,
@@ -10,6 +12,16 @@ export default function Page() {
     date: new Date(2024, 6, 3),
     value: 15300000
   }
+
+  const transaction2 = {
+    id: 1,
+    image: "https://eianmciufswbutirdbka.supabase.co/storage/v1/object/public/my%20files/images/icons/bank.jpg?t=2024-03-03T11%3A56%3A20.199Z",
+    type: "INCOME",
+    category_name: 'Học tập',
+    name: "Mua sách tiếng Anh",
+    value: 15300000
+  }
+
   return (
     <View style={styles.container}>
       <Stack.Screen
@@ -19,6 +31,7 @@ export default function Page() {
         }}
       />
       <TransactionWithDate transaction={transaction} />
+      <TransactionWithName transaction={transaction2} />
       <Text>Index page of Widgets Tab</Text>
     </View>
   );
