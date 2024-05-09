5671c98095101ddd1d075ec22fbe4dd869c875f5
tamxtanh <tam16121612@gmail.com>
2024-05-09 17:10:14 +0700

    update sdk, update ui account

--- wise-money/app/(tabs)/account/index.js
+++ wise-money/app/(tabs)/account/index.js
@@ -1,9 +1,11 @@
 import { Stack } from "expo-router";
-import { Alert, StyleSheet, Text, View } from "react-native";
+import { Alert, ScrollView, StyleSheet, Text, View } from "react-native";
 import { useRouter } from "expo-router";
 import { Button } from "react-native-elements";
 import { supabase } from "../../../lib/supabase";
-import UpdateProfile from '../../../components/profile/UpdateProfile'
+import UpdateProfile from "../../../components/profile/UpdateProfile";
+import { COLORS, icons, SIZES } from "../../../constants";
+import UtilityItemList from "../../../components/utility/utilityItemList";
 
 export default function Page() {
   const router = useRouter();
@@ -18,15 +20,105 @@ export default function Page() {
     }
   };
 
+  const utilityList = [
+    {
+      title: "Wallets",
+      icon: <icons.wallet3 fill="white" />,
+      colorBox: "#4445E8",
+    },
+    {
+      title: "Categories",
+      icon: <icons.category fill="white" />,
+      colorBox: "#12C144",
+    },
+    {
+      title: "Debts",
+      icon: <icons.debts fill="white" />,
+      colorBox: "#FB6C23",
+    },
+    {
+      title: " Suggestions",
+      icon: <icons.spendingSuggestions fill="white" />,
+      colorBox: "#0585FF",
+    },
+
+    {
+      title: "Tax",
+      icon: <icons.spendingSuggestions fill="white" />,
+      colorBox: "#1BD6E2",
+    },
+
+    {
+      title: "Exchange Rates",
+      icon: <icons.exchangeRates fill="white" />,
+      colorBox: "#4445E8",
+    },
+
+    {
+      title: "Interest",
+      icon: <icons.bank fill="white" />,
+      colorBox: "#FF4133",
+    },
+    {
+      title: "Export Data",
+      icon: <icons.exportData fill="white" />,
+      colorBox: "#FFC75C",
+    },
+  ];
+
   return (
     <View style={styles.container}>
       <Stack.Screen
         options={{
-          headerTitle: "Account",
+          headerLeft: () => (
+            <View style={{ marginLeft: 5 }}>
+              <Text
+                style={{
+                  fontSize: 20,
+                  fontFamily: "InterSemiBold",
+                  color: "white",
+                }}
+              >
+                Account
+              </Text>
+            </View>
+          ),
+          headerTitle: "",
+          headerRight: () => (
+            <View style={{ flexDirection: "row", gap: 8 }}>
+              <Text
+                style={{
+                  fontSize: 15,
+                  fontFamily: "InterMedium",
+                  color: "white",
+                }}
+              >
+                Support
+              </Text>
+              <icons.support />
+            </View>
+          ),
+          headerStyle: {
+            backgroundColor: COLORS.primary,
+          },
+          headerShadowVisible: false,
         }}
       />
-      <UpdateProfile></UpdateProfile>
-      <Button title="Sign Out" onPress={signOut} />
+      <ScrollView
+        style={{ flex: 1, marginBottom: SIZES.heightBottomNavigation }}
+      >
+        <View style={{ flex: 1 }}>
+          <UpdateProfile></UpdateProfile>
+          <Button title="Sign Out" onPress={signOut} />
+
+          <View style={styles.otherUtilities}>
+            <Text style={styles.lTitleBox}> Other utilities</Text>
+            <View style={styles.listOtherUti}>
+              <UtilityItemList itemData={utilityList} qualityPerRow={4} />
+            </View>
+          </View>
+        </View>
+      </ScrollView>
     </View>
   );
 }
@@ -34,7 +126,17 @@ export default function Page() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+  },
+
+  otherUtilities: {
+    marginTop: 10,
+    backgroundColor: "white",
+    paddingVertical: 15,
+    paddingHorizontal: 10,
+  },
+  lTitleBox: {
+    fontSize: 16,
+    fontFamily: "InterSemiBold",
+    color: "#000000",
   },
 });
