d04a021fecf60d831b25e16d6224d3b6dcb0a8ad
Bow Jaro <100690848+BowJaro@users.noreply.github.com>
2024-06-08 15:12:10 +0700

    done Tax calculate page

---
+++ wise-money/app/tax/styles.tsx
@@ -0,0 +1,111 @@
+import { COLORS, FONT } from '../../constants';
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    scrollView: {
+        flex: 1,
+        backgroundColor: COLORS.background,
+    },
+    container: {
+        flex: 1,
+        padding: 20,
+        backgroundColor: COLORS.background,
+    },
+    component: {
+        marginBottom: 20,
+    },
+    title: {
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+    description: {
+        fontSize: 14,
+        color: '#666',
+        marginBottom: 10,
+    },
+    inputContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        position: 'relative',
+    },
+    input: {
+        flex: 5,
+        borderWidth: 1,
+        borderColor: COLORS.gray,
+        padding: 10,
+        borderRadius: 5,
+        paddingRight: 40,
+    },
+    inputIcon: {
+        position: 'absolute',
+        right: 10,
+        fontSize: 18,
+        color: '#666',
+    },
+    radioContainer: {
+        flexDirection: 'column',  // Change to column
+        justifyContent: 'flex-start',
+
+    },
+    radioOption: {
+        marginVertical: 5,  // Add vertical margin for spacing
+        padding: 10,        // Add padding for better touch area
+        borderWidth: 1,     // Add border width
+        borderColor: COLORS.gray, // Border color
+        borderRadius: 5,    // Add border radius
+        backgroundColor: COLORS.background, // Background color
+    },
+    radio: {
+        fontSize: 16,
+        color: COLORS.gray,
+        paddingVertical: 10,
+    },
+    selectedRadio: {
+        fontSize: 16,
+        color: COLORS.primary,
+        paddingVertical: 10,
+        fontWeight: 'bold',
+    },
+    calculateButton: {
+        backgroundColor: COLORS.primary,
+        borderRadius: 20,
+        width: '100%',
+        height: 'auto',
+        padding: 20,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    calculateButtonText: {
+        fontFamily: FONT.bold,
+        fontSize: 18,
+        color: COLORS.white,
+    },
+    summary: {
+        marginTop: 20,
+        padding: 10,
+        backgroundColor: '#f9f9f9',
+        borderRadius: 5,
+    },
+    summaryText: {
+        fontSize: 16,
+    },
+    summaryTitle: {
+        fontWeight: 'bold'
+    },
+    table: {
+        marginTop: 20,
+    },
+    tableRow: {
+        flexDirection: 'row',
+        paddingVertical: 10,
+    },
+    tableHeader: {
+        flex: 7,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    tableCell: {
+        flex: 7,
+        textAlign: 'center',
+    },
+});
